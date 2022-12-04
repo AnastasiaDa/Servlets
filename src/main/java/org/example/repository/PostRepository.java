5d254aba588d75f2ab36e4fa5e7b679086dc3bb0
AnastasiaDa <danilova12333@gmail.com>
2022-12-04 23:28:08 +0300

    repository was fixed

--- src/main/java/org/example/repository/PostRepository.java
+++ src/main/java/org/example/repository/PostRepository.java
@@ -2,24 +2,45 @@ package org.example.repository;
 
 import org.example.model.Post;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 
 // Stub
 public class PostRepository {
+
+    private Map<Long, Post> posts = new ConcurrentHashMap<>();
+    private AtomicLong idCount = new AtomicLong(0);
+
     public List<Post> all() {
-        return Collections.emptyList();
+        return new ArrayList<>(posts.values());
     }
 
     public Optional<Post> getById(long id) {
-        return Optional.empty();
+        if (posts.containsKey(id)) {
+            return Optional.of(posts.get(id));
+        } else {
+            return Optional.empty();
+        }
     }
 
     public Post save(Post post) {
+        if (post.getId() == 0) {
+            long newId = idCount.incrementAndGet();
+            post.setId(newId);
+            posts.put(newId, post);
+        } else {
+            if (getById(post.getId()).isPresent()) {
+                posts.put(post.getId(), post);
+            } else {
+                return null;
+            }
+        }
         return post;
     }
 
     public void removeById(long id) {
+        posts.remove(id);
     }
 }
